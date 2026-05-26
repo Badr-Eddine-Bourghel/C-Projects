968afe35a33ed9bfbe3e321e90ae246485017de4
Badr-Eddine-Bourghel <bourghel.badreddine@ensam-casa.ma>
2026-05-26 14:37:38 +0100

    function init_numbers(): transformes a sorted array of integers into a random one

--- Sort-Visualizer-C/visual-sort.c
+++ Sort-Visualizer-C/visual-sort.c
@@ -1,9 +1,10 @@
 #include <raylib.h>
+#include <stdlib.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 
-#define COUNT 10
+#define COUNT 50
 
 int numbers[COUNT];
 
@@ -17,11 +18,26 @@ void draw_bars() {
   };
 }
 
-int main() {
-
+void init_numbers() {
   for (int i = 0; i < COUNT; i++) {
     numbers[i] = i;
   }
+
+  // Fisher Yates Shuffer
+  for (int i = COUNT - 1; i >= 0; i--) {
+    int j = rand() % (i + 1); // Generates a random number between 0 and i
+
+    // Swap the array entries
+    int tmp = numbers[i];
+    numbers[i] = numbers[j];
+    numbers[j] = tmp;
+  }
+}
+
+int main() {
+
+  init_numbers();
+
   InitWindow(WIDTH, HEIGHT, "Soritng Visualizer");
 
   while (!WindowShouldClose()) {
