38726edacb2c1604190f495d6f50be1d283aaca7
Badr-Eddine-Bourghel <bourghel.badreddine@ensam-casa.ma>
2026-05-26 18:25:38 +0100

    coloring  selected bars in red while sorting and in skyblue when the sorting is finished

--- Sort-Visualizer-C/visual-sort.c
+++ Sort-Visualizer-C/visual-sort.c
@@ -1,20 +1,35 @@
 #include <raylib.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 
-#define COUNT 50
+#define COUNT 100
 
 int numbers[COUNT];
 
-void draw_bars() {
+typedef struct {
+  bool swapped;
+  int i, j; // bars that were swapped
+  bool done;
+} SortStatus;
+
+void draw_bars(SortStatus status) {
 
   for (int i = 0; i < COUNT; i++) {
     int value = numbers[i];
+
+    Color color = WHITE;
+    if (status.done) {
+      color = SKYBLUE;
+    } else if (status.swapped && (i == status.i || i == status.j)) {
+      color = RED;
+    }
+
     int bar_height = (value * HEIGHT * 0.75) / COUNT;
     DrawRectangle(((float)i / COUNT) * WIDTH, (HEIGHT * 0.75) - bar_height,
-                  WIDTH / COUNT - 2, bar_height, WHITE);
+                  WIDTH / COUNT - 2, bar_height, color);
   };
 }
 
@@ -39,22 +54,32 @@ void init_numbers() {
   }
 }
 
-void sort_step() {
-  static int i;
+SortStatus sort_step() {
+  static SortStatus status = (SortStatus){false, 0, 1, false};
+  static int i = 0;
 
-  if (i < COUNT - 1) {
+  status.i = i;
+  status.j = i + 1;
+  if (status.i < COUNT - 1) {
 
-    int current_value = numbers[i];
-    int next_value = numbers[i + 1];
+    int current_value = numbers[status.i];
+    int next_value = numbers[status.j];
 
     if (current_value > next_value) {
-      swap(i, i + 1);
+      swap(status.i, status.j);
+      status.swapped = true;
     }
     i++;
   } else {
+    if (!status.swapped) {
+      // done
+      status.done = true;
+    }
     // Reached the end of the array -> go back to 0 and sort again
     i = 0;
+    status.swapped = false;
   }
+  return status;
 }
 
 int main() {
@@ -63,15 +88,19 @@ int main() {
 
   InitWindow(WIDTH, HEIGHT, "Soritng Visualizer");
 
-  SetTargetFPS(COUNT);
+  SetTargetFPS(COUNT * 3);
+
+  SortStatus status;
   while (!WindowShouldClose()) {
 
-    sort_step();
+    if (!status.done) {
+      status = sort_step();
+    }
 
     BeginDrawing();
     ClearBackground(BLACK);
     // Draw numbers
-    draw_bars();
+    draw_bars(status);
 
     EndDrawing();
   }
