d5fab9e3e3c06817fd256a15adf1182701c7102e
Badr-Eddine-Bourghel <bourghel.badreddine@ensam-casa.ma>
2026-05-26 14:26:11 +0100

    Drawing bars accordine to a sorted array of numbers

--- Sort-Visualizer-C/visual-sort.c
+++ Sort-Visualizer-C/visual-sort.c
@@ -3,14 +3,32 @@
 #define WIDTH 900
 #define HEIGHT 600
 
+#define COUNT 10
+
+int numbers[COUNT];
+
+void draw_bars() {
+
+  for (int i = 0; i < COUNT; i++) {
+    int value = numbers[i];
+    int bar_height = (value * HEIGHT * 0.75) / COUNT;
+    DrawRectangle(((float)i / COUNT) * WIDTH, (HEIGHT * 0.75) - bar_height,
+                  WIDTH / COUNT - 2, bar_height, WHITE);
+  };
+}
+
 int main() {
 
+  for (int i = 0; i < COUNT; i++) {
+    numbers[i] = i;
+  }
   InitWindow(WIDTH, HEIGHT, "Soritng Visualizer");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
 
-    DrawRectangle(50, 50, 30, 100, WHITE);
+    // Draw numbers
+    draw_bars();
 
     EndDrawing();
   }
