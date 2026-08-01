73e7477af6b355c808cbb3470eb7ffc80d00d10f
Badr-Eddine-Bourghel <BOURGHEL.BADREDDINE@ensam-casa.ma>
2026-08-01 11:16:46 +0100

    Data structure & global state

--- Snake-Game/snake.c
+++ Snake-Game/snake.c
@@ -10,3 +10,22 @@
 #define GOLDEN_CHANCE     5  /* 1 in N food spawns is golden*/
 #define GOLDEN_LIFETIME   60 /* ticks before golden food expires*/
 #define HIGH_SCORE_FILE   ".snake_highscore"
+
+typedef struct {
+  int x, y;
+} Point;
+
+typedef enum {DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT} Direction;
+
+enum {
+  CP_SNAKE = 1,
+  CP_FOOD,
+  CP_GOLDEN,
+  CP_WALL,
+  CP_OBSTACLE,
+  CP_TEXT
+};
+
+static Point snake[MAX_SNAKE_LEN];
+static int snake_len;
+static Direction dir;
