d8739cb06c6830fcb03dcd2b95f48e759ad7f251
Badr-Eddine-Bourghel <BOURGHEL.BADREDDINE@ensam-casa.ma>
2026-08-01 20:07:53 +0100

    Color Pair Enum

--- System-Monitor/system_monitor.c
+++ System-Monitor/system_monitor.c
@@ -13,3 +13,16 @@
 #define MAX_PROCESSES 512
 #define MAX_PROC_NAME 256
 #define REFRESH_INTERVAl 1
+
+// Color pairs
+enum {
+  CP_HEADER = 1,
+  CP_CPU,
+  CP_MEM,
+  CP_SWAP,
+  CP_TEXT,
+  CP_WARNING,
+  CP_CRITICAL,
+  CP_BORDER,
+  CP_PROCESS,
+}
